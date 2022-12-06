00befd7d3b21a19c52402755519a2d36ab166d00
Velotome <noemarzat@gmail.com>
2022-12-06 11:47:29 +0100

    Day 5 ++

--- Day 5/1.py
+++ Day 5/1.py
@@ -1,32 +1,30 @@
 input = open("D:\\Code\Advent-of-code-2022\Day 5\input.txt", "r")
-total = 0
 
-stacks = []
-#Parsing
+def moveCrate(stacks, number_of_crate, start, end):
+    if len(stacks[1 + 4*(int(start)-1)])-int(number_of_crate) > 0:
+        for i in range(0,int(number_of_crate)-1):
+            crates_to_move.append(stacks[1 + 4*(int(start)-1)].pop())
+    
+        for i in range(0,int(number_of_crate)-1):
+            stacks[1 + 4*(int(end)-1)].append(crates_to_move.pop())
+
+stacks = {}
+crates_to_move = []
+
 for line in input:
+    #Parsing
     if '[' in line:
-        line = list(line.replace('[','').replace(']',''))
-        for i in range(0,len(line)-1):
-            if i%2 == 0:
-                stacks.append([line[i]])
+        for i in range(1,len(line)):
+            if i%4 == 1:
+                if i in stacks :
+                    stacks[i].append(line[i])
+                else:
+                    stacks[i] = [line[i]]
+
+    #moving crates
+    elif "move" in line:
+        line = line.replace("move", "").replace("from","").replace("to","").replace(" ","").replace("\n","")
+        moveCrate(stacks, line[0], line[1], line[2])
+
 
 print(stacks)
-
-
-"""
-['N', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'R', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'C', '\n']
-['T', ' ', 'J', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'S', ' ', 'J', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'N', '\n']
-['B', ' ', 'Z', ' ', ' ', ' ', ' ', ' ', 'H', ' ', 'M', ' ', 'Z', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'D', '\n']
-['S', ' ', 'P', ' ', ' ', ' ', ' ', ' ', 'G', ' ', 'L', ' ', 'H', ' ', 'Z', ' ', ' ', ' ', ' ', ' ', 'T', '\n']
-['Q', ' ', 'D', ' ', ' ', ' ', ' ', ' ', 'F', ' ', 'D', ' ', 'V', ' ', 'L', ' ', 'S', ' ', 'M', '\n']
-['H', ' ', 'F', ' ', 'V', ' ', 'J', ' ', 'C', ' ', 'W', ' ', 'P', ' ', 'W', ' ', 'L', '\n']
-['G', ' ', 'S', ' ', 'H', ' ', 'Z', ' ', 'Z', ' ', 'T', ' ', 'F', ' ', 'V', ' ', 'H', '\n']
-['R', ' ', 'H', ' ', 'Z', ' ', 'M', ' ', 'T', ' ', 'M', ' ', 'T', ' ', 'Q', ' ', 'W', '\n']
-"""
-
-"""
-[['N'], [' '], [' '], [' '], [' '], [' '], [' '], ['R'], [' '], [' '], [' '], [' '], [' '], [' '], ['C'],
-['T'], ['J'], [' '], [' '], [' '], [' '], ['S'], ['J'], [' '], [' '], [' '], [' '], ['N'],
-['B'], ['Z'], [' '], [' '], 
-['H'], ['M'], ['Z'], [' '], [' '], [' '], [' '], ['D'], ['S'], ['P'], [' '], [' '], ['G'], ['L'], ['H'], ['Z'], [' '], [' '], ['T'], ['Q'], ['D'], [' '], [' '], ['F'], ['D'], ['V'], ['L'], ['S'], ['M'], ['H'], ['F'], ['V'], ['J'], ['C'], ['W'], ['P'], ['W'], ['L'], ['G'], ['S'], ['H'], ['Z'], ['Z'], ['T'], ['F'], ['V'], ['H'], ['R'], ['H'], ['Z'], ['M'], ['T'], ['M'], ['T'], ['Q'], ['W']]
-"""
